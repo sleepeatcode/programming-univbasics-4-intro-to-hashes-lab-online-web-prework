31e853ad81239f84c1fac64876ca2d7dbc868ac3
Rachel T <rachmaybe@gmail.com>
2019-08-05 22:08:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,7 @@ def update_counting_hash(hash, key)
   else
      puts "Key Not Found"
      hash[key] = 1
+     return
   end 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
